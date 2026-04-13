878437701d2288db0860125d4a4e833519becff8
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-13 15:31:04 -0300

    Se establecieron los atributos y las etiquetas necesarias para sus métodos y su comportamiento en la BD.
    Product.java

--- src/main/java/com/api/ecommerce/products/domain/Product.java
+++ src/main/java/com/api/ecommerce/products/domain/Product.java
@@ -1,4 +1,26 @@
 package com.api.ecommerce.products.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Integer stock;
+    private BigDecimal unitPrice;
+    // Fotos
+    // Categorias
 }
