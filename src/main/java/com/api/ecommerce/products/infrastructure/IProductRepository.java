6a846a98d25a006d09ded877fdb477bc2da1f386
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-13 15:27:51 -0300

    Se definió la interfaz IProductRepository.java.

--- src/main/java/com/api/ecommerce/products/infrastructure/IProductRepository.java
+++ src/main/java/com/api/ecommerce/products/infrastructure/IProductRepository.java
@@ -1,4 +1,8 @@
 package com.api.ecommerce.products.infrastructure;
 
-public interface IProductRepository {
-}
+import com.api.ecommerce.products.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProductRepository extends JpaRepository<Product,Long> {}
