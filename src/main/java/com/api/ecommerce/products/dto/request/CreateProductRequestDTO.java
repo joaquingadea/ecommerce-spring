89a928f7ab1246713e655a36bfc59691c1eb46e6
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-15 17:06:23 -0300

    Creación de DTO correspondiente para la creación de productos.
    CreateProductRequestDTO.java

--- src/main/java/com/api/ecommerce/products/dto/request/CreateProductRequestDTO.java
+++ src/main/java/com/api/ecommerce/products/dto/request/CreateProductRequestDTO.java
@@ -1,4 +1,14 @@
 package com.api.ecommerce.products.dto.request;
+import org.springframework.web.multipart.MultipartFile;
+import java.math.BigDecimal;
+import java.util.Set;
 
-public record ProductRequestDTO() {
+public record CreateProductRequestDTO(
+        String name,
+        String description,
+        BigDecimal price,
+        Integer stock,
+        Set<MultipartFile> images,
+        Set<Long> categories
+) {
 }
