29f8ff8fb3d264fbf2f0bf7a06d0d648e30a6133
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-15 16:58:16 -0300

    Definición del endpoint create e iny de dependencias de la interfaz de servicio de product.
    ProductController.java

--- src/main/java/com/api/ecommerce/products/api/ProductController.java
+++ src/main/java/com/api/ecommerce/products/api/ProductController.java
@@ -1,4 +1,28 @@
 package com.api.ecommerce.products.api;
 
+import com.api.ecommerce.products.application.IProductService;
+import com.api.ecommerce.products.dto.request.CreateProductRequestDTO;
+import com.api.ecommerce.shared.web.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    private IProductService productService;
+
+    public ProductController(IProductService productService) {
+        this.productService = productService;
+    }
+
+    public ResponseEntity<ApiResponse> create(@RequestBody CreateProductRequestDTO requestDTO){
+        productService.create(requestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ApiResponse("Created product successfully!"));
+    }
 }
