e1ffec46f32761605b7bc6e4841cb13d6d8c6ba3
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-23 20:49:58 -0300

    Agregado de la annotation @Column al atributo name.
    ProductCategory.java

--- src/main/java/com/api/ecommerce/products/domain/ProductCategory.java
+++ src/main/java/com/api/ecommerce/products/domain/ProductCategory.java
@@ -12,5 +12,6 @@ public class ProductCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true,nullable = false)
     private String name;
 }
