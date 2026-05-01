6fcd14662594c2803ca692ce869f213e3437f3b9
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-30 21:30:56 -0300

    Creación e mplementación de los métodos getPublicProducts, getAllDataProducts, editById, deactivate y activate
    ProductService e IProductService

--- src/main/java/com/api/ecommerce/products/application/IProductService.java
+++ src/main/java/com/api/ecommerce/products/application/IProductService.java
@@ -1,13 +1,21 @@
 package com.api.ecommerce.products.application;
 
+import com.api.ecommerce.products.dto.request.AdminProductFilter;
 import com.api.ecommerce.products.dto.request.CreateProductDTO;
+import com.api.ecommerce.products.dto.request.EditProductDTO;
 import com.api.ecommerce.products.dto.request.ProductFilter;
+import com.api.ecommerce.products.dto.response.AllDataProductDTO;
 import com.api.ecommerce.products.dto.response.ProductSinglePageDTO;
+import com.api.ecommerce.products.dto.response.PublicProductDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface IProductService {
     void create(CreateProductDTO requestDTO);
     ProductSinglePageDTO getSinglePageProductById(Long id);
-    Page<ProductSinglePageDTO> getProducts(ProductFilter filter, Pageable pageRequest);
+    Page<PublicProductDTO> getPublicProducts(ProductFilter filter, Pageable pageRequest);
+    Page<AllDataProductDTO> getAllDataProducts(AdminProductFilter filter, Pageable pageRequest);
+    void editById(Long id, EditProductDTO requestDTO);
+    void deactivate(Long id);
+    void activate(Long id);
 }
--- src/main/java/com/api/ecommerce/products/application/ProductService.java
+++ src/main/java/com/api/ecommerce/products/application/ProductService.java
@@ -3,13 +3,21 @@ package com.api.ecommerce.products.application;
 import com.api.ecommerce.products.domain.Product;
 import com.api.ecommerce.products.domain.ProductCategory;
 import com.api.ecommerce.products.domain.ProductImage;
+import com.api.ecommerce.products.domain.ProductStatus;
+import com.api.ecommerce.products.dto.request.AdminProductFilter;
 import com.api.ecommerce.products.dto.request.CreateProductDTO;
+import com.api.ecommerce.products.dto.request.EditProductDTO;
 import com.api.ecommerce.products.dto.request.ProductFilter;
+import com.api.ecommerce.products.dto.response.AllDataProductDTO;
 import com.api.ecommerce.products.dto.response.ProductSinglePageDTO;
+import com.api.ecommerce.products.dto.response.PublicProductDTO;
 import com.api.ecommerce.products.infrastructure.persistence.IProductCategoryRepository;
+import com.api.ecommerce.products.infrastructure.persistence.IProductImageRepository;
 import com.api.ecommerce.products.infrastructure.persistence.IProductRepository;
+import com.api.ecommerce.products.infrastructure.persistence.specification.AdminProductSpecBuilder;
 import com.api.ecommerce.products.infrastructure.persistence.specification.ProductSpecificationBuilder;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,11 +25,15 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
+@Transactional
 public class ProductService implements IProductService{
 
     @Value("app.url")
@@ -30,11 +42,13 @@ public class ProductService implements IProductService{
     private IProductRepository productRepository;
     private FileStorageService fileStorageService;
     private IProductCategoryRepository categoryRepository;
+    private IProductImageRepository imageRepository;
 
-    public ProductService(IProductRepository productRepository, FileStorageService fileStorageService, IProductCategoryRepository categoryRepository) {
+    public ProductService(IProductRepository productRepository, FileStorageService fileStorageService, IProductCategoryRepository categoryRepository, IProductImageRepository imageRepository) {
         this.productRepository = productRepository;
         this.fileStorageService = fileStorageService;
         this.categoryRepository = categoryRepository;
+        this.imageRepository = imageRepository;
     }
 
     public ProductImage mapToImage(MultipartFile file, Product newProduct){
@@ -49,15 +63,18 @@ public class ProductService implements IProductService{
                 requestDTO.description(),
                 requestDTO.stock(),
                 requestDTO.price(),
+                0,
                 null,
-                null);
+                null,
+                ProductStatus.ACTIVE
+        );
 
-        Set<ProductImage> images = requestDTO.images()
+        List<ProductImage> images = requestDTO.images()
                 .stream().map(file -> mapToImage(file,newProduct))
-                .collect(Collectors.toSet());
+                .toList();
 
-        Set<ProductCategory> categories =
-                new HashSet<>(categoryRepository.findAllById(requestDTO.categories()));
+        List<ProductCategory> categories =
+                new ArrayList<>(categoryRepository.findAllById(requestDTO.categories()));
 
         newProduct.setImages(images);
         newProduct.setProductCategories(categories);
@@ -65,6 +82,7 @@ public class ProductService implements IProductService{
         productRepository.save(newProduct);
     }
 
+    record longAndString(Long id, String text){}
     @Override
     public ProductSinglePageDTO getSinglePageProductById(Long id) {
         return productRepository.findById(id)
@@ -75,36 +93,81 @@ public class ProductService implements IProductService{
                         product.getUnitPrice(),
                         product.getStock(),
                         product.getProductCategories().stream()
-                                .map(ProductCategory::getName).toList(),
+                                .map(category -> new longAndString(category.getId(),category.getName())).toList(),
                         product.getImages().stream()
-                                .map(image -> urlApp + image.getUrl()).toList()
+                                .map(image -> new longAndString(image.getId(), urlApp + image.getUrl())).toList()
                 ))
-                .orElseThrow(EntityNotFoundException::new);
+                .orElseThrow(() -> new EntityNotFoundException("Product not found"));
     }
 
-    record idNameDTO(Long id, String name){}
     @Override
-    public Page<ProductSinglePageDTO> getProducts(ProductFilter filter, Pageable pageRequest) {
+    public Page<PublicProductDTO> getPublicProducts(ProductFilter filter, Pageable pageRequest) {
         Specification<Product> spec = ProductSpecificationBuilder.build(filter);
-        return productRepository.findAll(spec,pageRequest)
-                .map(product -> new ProductSinglePageDTO(
-                        product.getId(),
-                        product.getName(),
-                        product.getDescription(),
-                        product.getUnitPrice(),
-                        product.getStock(),
-                        product.getProductCategories().stream().map(
-                                category -> new idNameDTO(
-                                        category.getId(),
-                                        category.getName()
-                                )
-                        ).toList(),
-                        product.getImages().stream().map(image -> new idNameDTO(
-                                image.getId(),
-                                image.getUrl()
-                            )
-                        ).toList()
-                ));
+        return productRepository.findBy(
+                spec,query ->
+                query.as(PublicProductDTO.class)
+                        .page(pageRequest)
+        );
+    }
+
+    @Override
+    public Page<AllDataProductDTO> getAllDataProducts(AdminProductFilter filter, Pageable pageRequest) {
+        Specification<Product> spec = AdminProductSpecBuilder.build(filter);
+        return productRepository.findBy(spec,query ->
+                    query.as(AllDataProductDTO.class)
+                            .page(pageRequest)
+                );
+    }
+
+    @Override
+    public void editById(Long id, EditProductDTO requestDTO) {
+        Product productRepo = productRepository.findById(id).orElseThrow();
+        productRepo.setName(requestDTO.name());
+        productRepo.setDescription(requestDTO.description());
+        productRepo.setUnitPrice(requestDTO.price());
+        productRepo.setStock(requestDTO.stock());
+
+        List<ProductCategory> categories = categoryRepository.findAllById(requestDTO.categories());
+        if (categories.size() != requestDTO.categories().size()) {
+            throw new EntityNotFoundException("Category not found");
+        }
+
+        productRepo.setProductCategories(categories);
+
+        if (requestDTO.deleteImages() != null && !requestDTO.deleteImages().isEmpty()){
+            productRepo.getImages().removeIf((image) -> {
+                   boolean shouldDelete = requestDTO.deleteImages().contains(image.getId());
+                   if(shouldDelete){
+                        fileStorageService.deleteImage(image.getUrl());
+                   }
+                   return shouldDelete;
+            });
+        }
+        if (requestDTO.newImages() != null && !requestDTO.newImages().isEmpty()){
+            for (MultipartFile file : requestDTO.newImages()) {
+                String savedPath = fileStorageService.saveFile(file);
+
+                ProductImage image = new ProductImage();
+                image.setUrl(savedPath);
+                image.setProduct(productRepo);
+
+                productRepo.getImages().add(image);
+            }
+        }
+
+        productRepository.save(productRepo);
+    }
+
+    @Override
+    public void deactivate(Long id) {
+        Product deactivateProduct = productRepository.findById(id).orElseThrow();
+        deactivateProduct.setStatus(ProductStatus.INACTIVE);
+    }
+
+    @Override
+    public void activate(Long id) {
+        Product activateProduct = productRepository.findById(id).orElseThrow();
+        activateProduct.setStatus(ProductStatus.ACTIVE);
     }
 
 
