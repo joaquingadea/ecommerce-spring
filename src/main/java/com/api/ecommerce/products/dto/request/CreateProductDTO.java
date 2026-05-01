4a47770f4b8d69416b127c493e75199ca517fc03
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-30 21:36:12 -0300

    Cambio de de Set a List en ambas colecciones.
    CreateProductDTO.java

--- src/main/java/com/api/ecommerce/products/dto/request/CreateProductDTO.java
+++ src/main/java/com/api/ecommerce/products/dto/request/CreateProductDTO.java
@@ -1,14 +1,14 @@
 package com.api.ecommerce.products.dto.request;
 import org.springframework.web.multipart.MultipartFile;
 import java.math.BigDecimal;
-import java.util.Set;
+import java.util.List;
 
 public record CreateProductDTO(
         String name,
         String description,
         BigDecimal price,
         Integer stock,
-        Set<MultipartFile> images,
-        Set<Long> categories
+        List<MultipartFile> images,
+        List<Long> categories
 ) {
 }
