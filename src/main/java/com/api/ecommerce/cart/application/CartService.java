993b6ffef5eb6cd9fd2a557f9b15cbe6cb8d0fc6
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-05-05 17:00:51 -0300

    Creación e implementación de los métodos increaseItem, decreaseItem, getCart, getTotal y deleteItem
    CartService e ICartService

--- src/main/java/com/api/ecommerce/cart/application/CartService.java
+++ src/main/java/com/api/ecommerce/cart/application/CartService.java
@@ -1,12 +1,125 @@
 package com.api.ecommerce.cart.application;
 
+import com.api.ecommerce.cart.domain.Cart;
+import com.api.ecommerce.cart.domain.CartItem;
+import com.api.ecommerce.cart.dto.response.CartItemDTO;
+import com.api.ecommerce.cart.infrastructure.ICartItemRepository;
+import com.api.ecommerce.cart.infrastructure.ICartRepository;
+import com.api.ecommerce.products.domain.Product;
+import com.api.ecommerce.products.infrastructure.persistence.IProductRepository;
+import com.api.ecommerce.users.domain.AppUser;
+import com.api.ecommerce.users.infrastructure.persistence.IAppUserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Service
+@Transactional
 public class CartService implements ICartService{
-    
+
+    private IAppUserRepository userRepository;
+    private IProductRepository productRepository;
+    private ICartItemRepository cartItemRepository;
+    private ICartRepository cartRepository;
+
+    public CartService(IAppUserRepository userRepository, IProductRepository productRepository, ICartItemRepository cartItemRepository, ICartRepository cartRepository) {
+        this.userRepository = userRepository;
+        this.productRepository = productRepository;
+        this.cartItemRepository = cartItemRepository;
+        this.cartRepository = cartRepository;
+    }
+
+    public CartItem getOrCreateCartItem(Cart cart, Product product){
+        return cart.getItems().stream()
+                .filter(item -> item.getProduct().getId().equals(product.getId()))
+                .findFirst()
+                .orElseGet(() -> {
+                    CartItem newItem = new CartItem(null,0,cart,product);
+                    cart.getItems().add(newItem);
+                    return newItem;
+                });
+    }
+
     @Override
     public void addItem(Long productId,Integer quantity,Long userId) {
-        
+        Product product = productRepository.findById(productId).orElseThrow();
+        Cart userCart = userRepository.findById(userId).orElseThrow().getUserCart();
+        CartItem item = getOrCreateCartItem(userCart,product);
+
+        if (quantity < 1 || quantity > product.getStock()) {
+            throw new RuntimeException("Invalid quantity!");
+        }
+        // si el usuario ya tenia almacenado en el carrito una cantidad, esto no lo sobrescribe
+        Integer newQuantity = quantity + item.getQuantity();
+        if (newQuantity > product.getStock()){
+            throw new RuntimeException("Invalid quantity!");
+        }
+        item.setQuantity(newQuantity);
     }
+    @Override
+    public void increaseItem(Long itemId, Long userId){
+        applyQuantityDelta(itemId,1,userId);
+    }
+    @Override
+    public void decreaseItem(Long itemId, Long userId){
+        applyQuantityDelta(itemId,-1,userId);
+    }
+
+    @Override
+    public List<CartItemDTO> getCart(Long userId) {
+        return userRepository.findById(userId).orElseThrow().getUserCart().getItems().stream()
+                .map(item -> new CartItemDTO(
+                        item.getId(),
+                        item.getQuantity(),
+                        item.getProduct().getId(),
+                        item.getProduct().getName(),
+                        item.getProduct().getStock(),
+                        item.getProduct().getUnitPrice()
+                )).toList();
+    }
+
+    @Override
+    public BigDecimal getTotal(Long userId) {
+        AppUser user = userRepository.findById(userId).orElseThrow();
+        BigDecimal total = BigDecimal.ZERO;
+        user.getUserCart()
+            .getItems()
+            .forEach(item ->
+                    total.add(item.getProduct().getUnitPrice()
+                            .multiply(BigDecimal.valueOf(item.getQuantity()))));
+        return total;
+    }
+
+    @Override
+    public void deleteItem(Long itemId, Long userId) {
+        CartItem item = cartItemRepository.findById(itemId).orElseThrow();
+        if (!item.getCart().getUser().getId().equals(userId)){
+            throw new RuntimeException("Unauthorized!");
+        }
+        Cart cart = item.getCart();
+        cart.getItems().remove(item);
+    }
+
+    // Delta en matematica representa una cambio finito, incremento o diferencia en una variable
+    public void applyQuantityDelta(Long itemId, Integer quantity,Long userId) {
+        CartItem item = cartItemRepository.findById(itemId).orElseThrow();
+        if (!item.getCart().getUser().getId().equals(userId)){
+            throw new RuntimeException("Unauthorized!");
+        }
+        int newQuantity = item.getQuantity() + quantity;
+        if (newQuantity == 0){
+            Cart cart = item.getCart();
+            cart.getItems().remove(item);
+            return;
+        }
+        if(newQuantity > 0 && newQuantity <= item.getProduct().getStock()){
+            item.setQuantity(newQuantity);
+        }
+        else {
+            throw new RuntimeException("Invalid quantity change!");
+        }
+    }
+
 }
--- src/main/java/com/api/ecommerce/cart/application/ICartService.java
+++ src/main/java/com/api/ecommerce/cart/application/ICartService.java
@@ -1,5 +1,16 @@
 package com.api.ecommerce.cart.application;
 
+import com.api.ecommerce.cart.dto.response.CartItemDTO;
+import org.jspecify.annotations.Nullable;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ICartService {
     void addItem(Long productId,Integer quantity, Long userId);
+    void increaseItem(Long itemId, Long userId);
+    void decreaseItem(Long itemId, Long userId);
+    List<CartItemDTO> getCart(Long userId);
+    BigDecimal getTotal(Long userId);
+    void deleteItem(Long itemId,Long userId);
 }
