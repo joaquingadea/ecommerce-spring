b8393e75fb976507eee882a5e1f3e247119ca016
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-05-11 18:56:57 -0300

    Creación de métodos findByUsername y existsByUsername
    IAppUseRepository.java

--- src/main/java/com/api/ecommerce/users/infrastructure/persistence/IAppUserRepository.java
+++ src/main/java/com/api/ecommerce/users/infrastructure/persistence/IAppUserRepository.java
@@ -9,10 +9,14 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IAppUserRepository extends JpaRepository<AppUser,Long>, JpaSpecificationExecutor<AppUser> {
     @Query("""
             SELECT u.id AS id, u.username AS username FROM AppUser u
             """)
     Page<UserIdUsernameDTO> findAllIdAndUsername(Pageable pageRequest);
+    Optional<AppUser> findByUsername(String username);
+    boolean existsByUsername(String username);
 }
