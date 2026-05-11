a0d8acabe5f2b3d453f8b67856b65cffb0a76749
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-05-11 19:42:20 -0300

    Creación e implementación de los métodos addCookie y deleteCookie
    CookieService.java

--- src/main/java/com/api/ecommerce/shared/security/cookie/CookieService.java
+++ src/main/java/com/api/ecommerce/shared/security/cookie/CookieService.java
@@ -1,7 +1,31 @@
 package com.api.ecommerce.shared.security.cookie;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CookieService {
+    public void addCookie(String cookieName, Object cookieValue, HttpServletResponse response, int maxAge) {
+        ResponseCookie cookie = ResponseCookie.from(cookieName,cookieValue.toString())
+                .secure(true)
+                .httpOnly(true)
+                .sameSite("None")
+                .maxAge(maxAge)
+                .path("/")
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE,cookie.toString());
+    }
+    public void deleteCookie(String cookieName,HttpServletResponse response){
+        ResponseCookie cookie = ResponseCookie.from(cookieName,null)
+                .secure(true)
+                .httpOnly(true)
+                .maxAge(0)
+                .sameSite("None")
+                .path("/")
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE,cookie.toString());
+    }
+
 }
