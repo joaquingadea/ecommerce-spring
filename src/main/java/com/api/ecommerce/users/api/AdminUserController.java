f5b351eb0c309ee02db7bf847d38711fb8065b1a
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-04 00:59:54 -0300

    Creación de endpoints para obtencion y creación de roles y permisos
    AdminUserController.java

--- src/main/java/com/api/ecommerce/users/api/AdminUserController.java
+++ src/main/java/com/api/ecommerce/users/api/AdminUserController.java
@@ -1,6 +1,10 @@
 package com.api.ecommerce.users.api;
 
 import com.api.ecommerce.users.application.IAppUserService;
+import com.api.ecommerce.users.application.IPermissionService;
+import com.api.ecommerce.users.application.IRoleService;
+import com.api.ecommerce.users.domain.Permission;
+import com.api.ecommerce.users.domain.Role;
 import com.api.ecommerce.users.dto.response.UserIdUsernameDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -8,18 +12,22 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/admin/user")
 public class AdminUserController {
 
     private IAppUserService appUserService;
+    private IRoleService roleService;
+    private IPermissionService permissionService;
 
-    public AdminUserController(IAppUserService appUserService) {
+    public AdminUserController(IAppUserService appUserService, IRoleService roleService, IPermissionService permissionService) {
         this.appUserService = appUserService;
+        this.roleService = roleService;
+        this.permissionService = permissionService;
     }
 
     @GetMapping("/customer-list")
@@ -27,5 +35,27 @@ public class AdminUserController {
         Pageable pageRequest = PageRequest.of(pageable.getPageNumber(),pageable.getPageSize());
         return ResponseEntity.status(HttpStatus.OK)
                 .body(appUserService.findAllIdAndUsername(pageRequest));
+    }
+
+
+    @GetMapping("/get-roles")
+    public ResponseEntity<List<Role>> getRoles(){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(roleService.findAll());
+    }
+    @PostMapping("/create-role")
+    public ResponseEntity<Role> createRole(@RequestBody Role role){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(roleService.create(role));
+    }
+    @GetMapping("/get-permissions")
+    public ResponseEntity<List<Permission>> getPermissions(){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(permissionService.findAll());
+    }
+    @PostMapping("/create-permission")
+    public ResponseEntity<Permission> createPermission(@RequestBody Permission permission){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(permissionService.create(permission));
     }
 }
