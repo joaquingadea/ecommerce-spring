cf14863b71d74cce5c82b2986ef41673fcda3b76
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-04 04:54:04 -0300

    Implementación del endpoint /get-all-data-product/{id}
    AdminProductController.java

--- src/main/java/com/api/ecommerce/products/api/AdminProductController.java
+++ src/main/java/com/api/ecommerce/products/api/AdminProductController.java
@@ -2,7 +2,9 @@ package com.api.ecommerce.products.api;
 
 import com.api.ecommerce.products.application.IProductCategoryService;
 import com.api.ecommerce.products.application.IProductService;
+import com.api.ecommerce.products.domain.ProductCategory;
 import com.api.ecommerce.products.dto.request.AdminProductFilter;
+import com.api.ecommerce.products.dto.request.CreateProductCategoryDTO;
 import com.api.ecommerce.products.dto.request.CreateProductDTO;
 import com.api.ecommerce.products.dto.request.EditProductDTO;
 import com.api.ecommerce.products.dto.response.AllDataProductDTO;
@@ -18,9 +20,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -35,16 +41,53 @@ public class AdminProductController {
         this.productCategoryService = productCategoryService;
     }
 
-    @PostMapping("/create-product")
+    /*@PostMapping("/create-product")
     public ResponseEntity<ApiResponse> createProduct(@RequestBody CreateProductDTO requestDTO){
         productService.create(requestDTO);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ApiResponse("Product successfully created!"));
+    }*/
+
+    @PostMapping(value = "/create-product", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> createProduct(
+            @RequestParam("name") String name,
+            @RequestParam("description") String description,
+            @RequestParam("price") BigDecimal unitPrice,
+            @RequestParam("stock") Integer stock,
+            @RequestParam("categoryId") Long categoryId,
+            @RequestParam("images") List<MultipartFile> images
+    ) {
+        List<Long> ids = new ArrayList<>();
+        ids.add(categoryId);
+        CreateProductDTO request = new CreateProductDTO(
+                name,
+                description,
+                unitPrice,
+                stock,
+                images,
+                ids
+        );
+        productService.create(request);
+        return ResponseEntity.ok().build();
     }
 
+    @GetMapping("/get-categories")
+    public ResponseEntity<Page<ProductCategory>> getCategories(@PageableDefault Pageable pageable){
+        Pageable pageRequest = PageRequest.of(pageable.getPageNumber(),pageable.getPageSize());
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(productCategoryService.getAll(pageRequest));
+    }
+
+    @GetMapping("/get-category-list")
+    public ResponseEntity<List<ProductCategory>> getCategoryList(){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(productCategoryService.getCategoryList());
+    }
+
+
     @PostMapping("/create-product-category")
-    public ResponseEntity<ApiResponse> createProductCategory(@RequestBody String requestCategory){
-        productCategoryService.create(requestCategory);
+    public ResponseEntity<ApiResponse> createProductCategory(@RequestBody CreateProductCategoryDTO request){
+        productCategoryService.create(request.getName());
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ApiResponse("Product category successfully created!"));
     }
@@ -60,21 +103,28 @@ public class AdminProductController {
     public ResponseEntity<Page<AllDataProductDTO>> getProducts(
             @RequestParam(required = false) String search,
             @RequestParam(required = false) List<Long> categoryIds,
-            @RequestParam(required = false) String sort,
+            @RequestParam(required = false) String sortByPrice,
             @RequestParam(required = false) Boolean active,
             @RequestParam(required = false) Boolean bestSellers,
             @RequestParam(required = false) Boolean hasStock,
+            @RequestParam(required = false) String sortByDate,
             @PageableDefault(size = 10,page = 0) Pageable pageable) throws BadRequestException {
 
         AdminProductFilter adminFilter = new AdminProductFilter(search,categoryIds,active,bestSellers,hasStock);
-        Sort sortPageRequest = ProductSortBuilder.build(sort,bestSellers);
+        Sort sortPageRequest = ProductSortBuilder.build(sortByPrice,bestSellers,sortByDate);
         Pageable pageRequest = PaginationConstants.validatePageable(pageable.getPageNumber(),pageable.getPageSize(),sortPageRequest);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(productService.getAllDataProducts(adminFilter,pageRequest));
 
     }
-    @PutMapping("/edit-product/{id}")
-    public ResponseEntity<ApiResponse> editProductById(@PathVariable Long id, @RequestBody EditProductDTO requestDTO){
+    @GetMapping("/get-all-data-product/{id}")
+    public ResponseEntity<AllDataProductDTO> getAllDataProduct(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(productService.getAllDataProduct(id));
+    }
+
+    @PostMapping(value = "/edit-product/{id}" , consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ApiResponse> editProductById(@PathVariable Long id, @ModelAttribute EditProductDTO requestDTO){
         productService.editById(id,requestDTO);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new ApiResponse("Product successfully edited!"));
