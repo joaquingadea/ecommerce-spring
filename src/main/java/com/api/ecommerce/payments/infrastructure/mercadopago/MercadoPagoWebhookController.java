4a5533fb58f666e95de3b083d5718bed068bea9e
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-07-06 14:57:30 -0300

    Utilización del método receiveWebhook y eliminación de comentarios
    MercadoPagoWebhookController

--- src/main/java/com/api/ecommerce/payments/infrastructure/mercadopago/MercadoPagoWebhookController.java
+++ src/main/java/com/api/ecommerce/payments/infrastructure/mercadopago/MercadoPagoWebhookController.java
@@ -15,12 +15,8 @@ public class MercadoPagoWebhookController {
     public MercadoPagoWebhookController(MercadoPagoWebhookService webhookService) {
         this.webhookService = webhookService;
     }
-    @GetMapping
-    public ResponseEntity<String> test() {
-        System.out.println("GET RECIBIDO");
-        return ResponseEntity.ok("OK");
-    }
-    /*@PostMapping
+    
+    @PostMapping
     public ResponseEntity<Void> receiveWebhook(HttpServletRequest request, @RequestBody(required = false) MercadoPagoNotificationDTO notification){
         try {
             //validate(request);
@@ -32,12 +28,5 @@ public class MercadoPagoWebhookController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .build();
         }
-    }*/
-    @PostMapping // ENDPOINT DE TEST PARA VER SI FUNCIONA LA CONEXION CON LA API
-    public ResponseEntity<Void> receiveWebhook(
-            @RequestBody(required = false) String body) {
-
-        System.out.println(body);
-        return ResponseEntity.ok().build();
     }
 }
