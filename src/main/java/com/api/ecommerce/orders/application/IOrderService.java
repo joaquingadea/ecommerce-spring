3f911fc6aa923d901b25e8cea858f63f49f12c66
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-07-06 14:31:01 -0300

    Agregado de los métodos createOrder, getMyOrderDetails, cancelMyPendingOrder y getMyPendingOrder al contrato
    IOrderService

--- src/main/java/com/api/ecommerce/orders/application/IOrderService.java
+++ src/main/java/com/api/ecommerce/orders/application/IOrderService.java
@@ -3,7 +3,7 @@ package com.api.ecommerce.orders.application;
 import com.api.ecommerce.cart.domain.Cart;
 import com.api.ecommerce.orders.domain.Order;
 import com.api.ecommerce.orders.dto.response.MyOrderDTO;
-import com.api.ecommerce.orders.dto.response.MyOrderDetailsDTO;
+import com.api.ecommerce.orders.dto.response.MyOrderDetailDTO;
 import com.api.ecommerce.orders.dto.response.OrderDTO;
 import com.api.ecommerce.orders.dto.response.OrderDetailDTO;
 import org.springframework.data.domain.Page;
@@ -12,10 +12,11 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface IOrderService {
-    Order getOrCreateOrder(Long userId,Cart cart);
+    Order createOrder(Long userId,Cart cart);
     Page<OrderDTO> getOrders(Pageable ordersPageable);
     List<OrderDetailDTO> getOrderDetails(Long orderId);
     Page<MyOrderDTO> getMyOrders(Long userId, Pageable pageRequest);
-    MyOrderDTO getMyPendingOrder(Long userId);
-    List<MyOrderDetailsDTO> getMyOrderDetails(Long orderId, Long aLong);
+    List<MyOrderDetailDTO> getMyOrderDetails(Long orderId, Long aLong);
+    void cancelMyPendingOrder(Long orderId,Long userId);
+    Order getPendingOrder(Long orderId, Long userId);
 }
