3f48fb29665793d096d94e12e592a5758f4718ef
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-07-06 14:43:29 -0300

    findByUserId -> findByIdAndUserId
    IOrderRepository

--- src/main/java/com/api/ecommerce/orders/infrastructure/persistence/IOrderRepository.java
+++ src/main/java/com/api/ecommerce/orders/infrastructure/persistence/IOrderRepository.java
@@ -7,7 +7,11 @@ import com.api.ecommerce.orders.dto.response.OrderDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -16,5 +20,5 @@ public interface IOrderRepository extends JpaRepository<Order,Long> {
     Optional<Order> findByUserIdAndStatus(Long userId, OrderStatus status);
     Page<OrderDTO> findAllProjectedBy(Pageable ordersPageable);
     Page<MyOrderDTO> findAllByUserId(Long userId,Pageable pageable);
-    MyOrderDTO findByUserId(Long userId);
+    Optional<Order> findByIdAndUserId(Long orderId, Long userId);
 }
